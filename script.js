239be2e264e08555983951d760b8e47887bd30e5
siep-cpu <taaleah.seep@gmail.com>
2026-05-28 23:34:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -72,14 +72,14 @@ function typeMessage() {
     }
 }
 const photos = [
-    "images/photo1.jpg",
-    "images/photo2.jpg",
-    "images/photo3.jpg",
-    "images/photo4.jpg",
-    "images/photo5.jpg",
-    "images/photo6.jpg",
-    "images/photo7.jpg",
-    "images/photo8.jpg"
+    "photo1.jpg",
+    "photo2.jpg",
+    "photo3.jpg",
+    "photo4.jpg",
+    "photo5.jpg",
+    "photo6.jpg",
+    "photo7.jpg",
+    "photo8.jpg"
 ];
 
 let photoIndex = 0;
